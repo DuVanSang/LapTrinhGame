f266ad37ca31d36b8f8d99ca67e33ed3cea15a86
Dư Văn Sang <sang25112005@gmail.com>
2026-03-15 15:13:45 +0700

    hoàn thiện demo 7

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -4,12 +4,15 @@ public class Health : MonoBehaviour
 {
     public GameObject explosionPrefab; 
     public int defaultHealthPoint = 3; 
-    protected int healthPoint;         
+    public int healthPoint;     
+
+    public System.Action onHealthChanged;    
     
     public System.Action onDead;
     private void Start()
     {
         healthPoint = defaultHealthPoint; 
+        onHealthChanged?.Invoke();
     }
 
     public void TakeDamage(int damage)
@@ -17,6 +20,7 @@ public class Health : MonoBehaviour
         if (healthPoint <= 0) return; 
 
         healthPoint -= damage;
+        onHealthChanged?.Invoke();
         if (healthPoint <= 0)
         {
             Die();
